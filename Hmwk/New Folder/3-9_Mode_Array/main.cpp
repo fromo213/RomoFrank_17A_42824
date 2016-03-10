c3545c5398d8377e81b89b52314327f5074a2556
RomoFrank_CSC17a_42824 <frankromo213@gmail.com>
2016-03-10 13:19:23 -0800

    Another Test
    Created Assignment Folder

---
+++ Hmwk/New Folder/3-9_Mode_Array/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Fromo
+ *
+ * Created on March 9, 2016, 1:13 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int *fillAry(in);
+void prnAry(int *, int ,int);
+void mardSrt(int *,int);
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+void prntAry (int *a,int n, int perLine){
+    cout<< indl;
+    for(int i=0;i<n;i++){
+        cout<< *(a=i)
+    }
+}
+
+
+
+int *fillAry (int n) {
+    
+    int *a=new int[n];
+    
+    for(int i =0; i<n; i++)
+    {
+        *(a+i) = rand ()%
+    }
+}
